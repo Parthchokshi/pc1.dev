9c9d6e3b10ae2184c8df86dfc7f60cb764bfd000
Parth Chokshi <parthchokshi1@gmail.com>
2019-02-24 16:25:26 -0500

    export to gatsby

---
+++ old/README.md
@@ -0,0 +1,2 @@
+# parth.plus
+Personal Portfolio Site using HTML5, CSS3 and Adobe Illustrator.
---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Style from "../styles/style.css"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Parth Chokshi" keywords={[`gatsby`, `application`, `react`]} />
+    <section className="main">
+      <div className="intro">
+        <div className="intro-heading-container">
+          <p className="upper-case intro-subheading">I am</p>
+          <h2 className="intro-heading">Parth Chokshi</h2>
+        </div>
+      </div>
+
+      <div className="work">
+        <div className="work-web">
+          <p className="work-web-title">Full Stack Developer</p>
+          <div className="work-pipe-web" />
+        </div>
+        <div className="work-avatar" />
+        <div className="work-mobile-design-container">
+          <div className="work-mobile">
+            <div className="work-pipe-mobile" />
+            <p className="work-mobile-title">Mobile Apps Developer</p>
+          </div>
+          <div className="work-design">
+            <div className="work-pipe-design" />
+            <p className="work-design-title">UI & UX Designer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="navigation">
+        <a className="navigation-connect" href="mailto:Parthchokshi1@gmail.com">
+          Connect
+        </a>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default IndexPage
